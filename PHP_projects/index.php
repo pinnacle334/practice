2892ef91f6e82f5c743f889310166e06f7639403
pinnacle334 <115135129+pinnacle334@users.noreply.github.com>
2024-01-24 16:38:48 +0100

    Update index.php

--- PHP_projects/index.php
+++ PHP_projects/index.php
@@ -7,6 +7,7 @@
 
 
 	echo "this will always be the main branch";
+	echo "I am editing this using GitHub editor!";
 
 	if ($sum % 2 == 0) {
 		echo "the sum of " . $x . " and " .$y . " is an even number";
